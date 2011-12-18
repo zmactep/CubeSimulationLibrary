a17efd78e4f7c3830e65f9590a307e62a0552dc9
MacTep <zhmactep@gmail.com>
2011-12-18 15:39:39 +0400

    Custom AgentManager infects :)

--- example/main.cpp
+++ example/main.cpp
@@ -1,4 +1,4 @@
-#include <QtGui>
+#include <QtGui/QApplication>
 #include <cubesimulationlibrary.h>
 
 #include "stupidagentmanagerfactory.h"
@@ -7,13 +7,12 @@ int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
   CubeSimulationLibrary lib;
-
   AgentManagerFactory *fact[2];
   fact[0] = new AgentManagerFactory;
   fact[1] = new StupidAgentManagerFactory;
 
-  lib.initScene("../example/map5.txt", fact, 2);
+  lib.initScene("../example/map8.txt", fact, 2);
   lib.showScene();
-  
+
   return a.exec();
 }
